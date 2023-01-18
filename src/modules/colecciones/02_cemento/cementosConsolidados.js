640847219841c840ea69b78e72ad3ddd2936d60a
H DLC <47111207+Enloch@users.noreply.github.com>
2023-01-18 15:21:56 +0100

    Arreglado en castellano

--- src/modules/colecciones/02_cemento/cementosConsolidados.js
+++ src/modules/colecciones/02_cemento/cementosConsolidados.js
@@ -15,43 +15,43 @@ export const KronGrey = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle2[3]}</>;
+  return <>{PagiCementoStyle2[1]}</>;
 };
 export const MomaArena = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[1]}</>;
+  return <>{PagiCementoStyle1[2]}</>;
 };
 export const MomaGris = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[2]}</>;
+  return <>{PagiCementoStyle1[3]}</>;
 };
 export const MoodAntra = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[6]}</>;
+  return <>{PagiCementoStyle1[4]}</>;
 };
 export const MoodGrey = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[7]}</>;
+  return <>{PagiCementoStyle1[5]}</>;
 };
 export const TitanAnt = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[4]}</>;
+  return <>{PagiCementoStyle1[6]}</>;
 };
 export const TitanCem = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <>{PagiCementoStyle1[5]}</>;
+  return <>{PagiCementoStyle1[7]}</>;
 };
 export const Vulcano = () => {
   useEffect(() => {
