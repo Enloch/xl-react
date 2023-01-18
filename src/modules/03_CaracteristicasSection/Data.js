81e3594b0eca8dd2b0312ec1f208bc3472a1cf47
H DLC <47111207+Enloch@users.noreply.github.com>
2023-01-18 16:08:57 +0100

    Correción faltas

--- src/modules/03_CaracteristicasSection/Data.js
+++ src/modules/03_CaracteristicasSection/Data.js
@@ -1,9 +1,9 @@
 export const caraObj = {
-  id: 'caracteristicas',
-  headLine: 'caracteristicas físico-mecánicas de XL //',
+  id: "caracteristicas",
+  headLine: "características  físico-mecánicas de XL //",
   description:
-    'Sus caracteristicas físico-mecánicas, permiten fabricar superficies con este material que cuentan con un sinfín de aplicaciones horizontales y decorativas. ',
-  img: require('../../assets/images/foto _iconos.webp'),
-  img2: require('../../assets/images/iconos.webp'),
-  alt: 'test',
-}
+    "Sus características  físico-mecánicas, permiten fabricar superficies con este material que cuentan con un sinfín de aplicaciones horizontales y decorativas. ",
+  img: require("../../assets/images/foto _iconos.webp"),
+  img2: require("../../assets/images/iconos.webp"),
+  alt: "test",
+};
--- src/modules/08_ColecSection/Data.js
+++ src/modules/08_ColecSection/Data.js
@@ -1,7 +1,7 @@
 export const colecObj = {
-  id: 'coleccion',
-  headLine: 'nuestras colecciones //',
+  id: "coleccion",
+  headLine: "nuestras colecciones //",
   description:
-    'Sorprendentes tanto a la vista como al tacto, nos permitirán construir cualquier espacio de nuestro hogar. Descubre las infinitas ideas y posiblilidades decorativas de nuestras colecciones XL.',
-  description2: 'ver nuestras colecciones //',
-}
+    "Sorprendentes tanto a la vista como al tacto, nos permitirán construir cualquier espacio de nuestro hogar. Descubre las infinitas ideas y posibilidades  decorativas de nuestras colecciones XL.",
+  description2: "ver nuestras colecciones //",
+};
