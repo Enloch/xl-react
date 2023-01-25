0adab2908ac734672dbdf1c9703fb17d81824f64
H DLC <47111207+Enloch@users.noreply.github.com>
2023-01-25 18:31:46 +0100

    25/01 Tooltip 45min

--- src/modules/bath/04_EncimeraBath/index.js
+++ src/modules/bath/04_EncimeraBath/index.js
@@ -28,6 +28,8 @@ import {
   ImgSelector2,
 } from "./encimeraStyle";
 import icono from "../../../assets/icons/botones/boton.png";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 import config from "./indicadores";
 const EncimeraBath = ({
   id,
@@ -47,96 +49,115 @@ const EncimeraBath = ({
     <>
       <ImgWrap>
         <IconContainer>
-          <Link to="" scroll="smooth">
+          <Link to={config[0].id} scroll="smooth">
             <Icono1
+              id={config[0].tooltip}
               src={icono}
               style={{
                 top: config[0].top,
                 left: config[0].left,
               }}
             />
+            <Tooltip anchorId={config[0].tooltip} content={config[0].tooltip} />
           </Link>
           <Link to={config[1].id} scroll="smooth">
             <Icono1
+              id={config[1].tooltip}
               src={icono}
               style={{
                 top: config[1].top,
                 left: config[1].left,
               }}
             />
+            <Tooltip anchorId={config[1].tooltip} content={config[1].tooltip} />
           </Link>
           <Link to={config[2].id} scroll="smooth">
             <Icono1
+              id={config[2].tooltip}
               src={icono}
               style={{
                 top: config[2].top,
                 left: config[2].left,
               }}
             />
+            <Tooltip anchorId={config[2].tooltip} content={config[2].tooltip} />
           </Link>
           <Link to={config[3].id} scroll="smooth">
             <Icono1
+              id={config[3].tooltip}
               src={icono}
               style={{
                 top: config[3].top,
                 left: config[3].left,
               }}
             />
+            <Tooltip anchorId={config[3].tooltip} content={config[3].tooltip} />
           </Link>
           <Link to={config[4].id} scroll="smooth">
             <Icono1
+              id={config[4].tooltip}
               src={icono}
               style={{
                 top: config[4].top,
                 left: config[4].left,
               }}
             />
+            <Tooltip anchorId={config[4].tooltip} content={config[4].tooltip} />
           </Link>
           <Link to={config[5].id} scroll="smooth">
             <Icono1
+              id={config[5].tooltip}
               src={icono}
               style={{
                 top: config[5].top,
                 left: config[5].left,
               }}
             />
+            <Tooltip anchorId={config[5].tooltip} content={config[5].tooltip} />
           </Link>
-          {/* <Link to={config[6].id} scroll="smooth">
+          <Link to={config[6].id} scroll="smooth">
             <Icono1
+              id={config[6].tooltip}
               src={icono}
               style={{
                 top: config[6].top,
                 left: config[6].left,
               }}
             />
-          </Link> */}
+            <Tooltip anchorId={config[6].tooltip} content={config[6].tooltip} />
+          </Link>
           <Link to={config[7].id} scroll="smooth">
             <Icono1
+              id={config[7].tooltip}
               src={icono}
               style={{
                 top: config[7].top,
                 left: config[7].left,
               }}
             />
+            <Tooltip anchorId={config[7].tooltip} content={config[7].tooltip} />
           </Link>
           <Link to={config[8].id} scroll="smooth">
             <Icono1
+              id={config[8].tooltip}
               src={icono}
               style={{
                 top: config[8].top,
                 left: config[8].left,
               }}
             />
+            <Tooltip anchorId={config[8].tooltip} content={config[8].tooltip} />
           </Link>
-
           <Link to={config[9].id} scroll="smooth">
             <Icono1
+              id={config[9].tooltip}
               src={icono}
               style={{
                 top: config[9].top,
                 left: config[9].left,
               }}
             />
+            <Tooltip anchorId={config[9].tooltip} content={config[9].tooltip} />
           </Link>
 
           <Img src={imgheader} alt={alt} />
--- src/modules/bath/04_EncimeraBath/indicadores.js
+++ src/modules/bath/04_EncimeraBath/indicadores.js
@@ -1,51 +1,61 @@
 const config = [
   {
     id: "forrado de modulos",
+    tooltip: "Forrado de módulos",
     top: "38%",
     left: "11%",
   },
   {
     id: "revestimiento",
+    tooltip: "Revestimiento",
     top: "60%",
     left: "23%",
   },
   {
     id: "xlshelf",
+    tooltip: "XL Shelf",
     top: "75.5% ",
     left: "19%",
   },
   {
     id: "frentes",
+    tooltip: "Frentes",
     top: "61%",
     left: "32%",
   },
   {
     id: "encimerabath",
+    tooltip: "Encimera Bath",
     top: "55%",
     left: "36%",
   },
   {
     id: "lavabos",
+    tooltip: "Lavabos",
     top: "58%",
     left: "47.7%",
   },
   {
     id: "xlspecial",
+    tooltip: "XL Special",
     top: "63.3%",
     left: "58.8%",
   },
   {
     id: "forradomuebles",
+    tooltip: "Forrado de muebles",
     top: "77%",
     left: "44%",
   },
   {
     id: "paneles de ducha",
+    tooltip: "Paneles de ducha",
     top: "50%",
     left: "65%",
   },
   {
     id: "platos de ducha",
+    tooltip: "Platos de ducha",
     top: "90%",
     left: "70%",
   },
