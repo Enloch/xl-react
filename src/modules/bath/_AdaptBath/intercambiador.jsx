422a57f7d68c996098209d4811f0a331fe46fd7b
H DLC <47111207+Enloch@users.noreply.github.com>
2023-01-31 17:34:57 +0100

    31/01 Set Actives

--- src/modules/bath/_AdaptBath/intercambiador.jsx
+++ src/modules/bath/_AdaptBath/intercambiador.jsx
@@ -10,10 +10,30 @@ import {
   MenuIntrucciones,
   BotonExpandir,
   BotonContinuar,
+  TextoIntro,
+  ContenedorIconos,
+  Icono1,
 } from "./intercambiadorStyles";
+import icono_b from "../../../assets/icons/botones/boton_b.png";
 import { frontales, modulos, laterales } from "./dataIntercambiador";
+const Elementos = [{ id: 1 }];
 const Intercambiador = ({ base }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [AllureVisible, SetAllure] = useState(false);
+  const [BerenVisibe, SetBeren] = useState(false);
+  const [blancborgVisible, SetBlancborg] = useState(false);
+  const [blancinvVisible, SetBlancinv] = useState(false);
+  const [brazilVisible, SetBrazil] = useState(false);
+  const [calacattaVisible, SetCalacatta] = useState(false);
+  const [emeVisible, SetEme] = useState(false);
+  const [kendocVisible, SetKendoc] = useState(false);
+  const [kendolVisible, SetKendol] = useState(false);
+  const [momaVisible, SetMoma] = useState(false);
+  const [negroVisible, SetNegro] = useState(false);
+  const [noonGVisible, SetNoonG] = useState(false);
+  const [noonsVisible, SetNoons] = useState(false);
+  const [paladioVisible, SetPaladio] = useState(false);
+  const [syracVisible, SetSyrac] = useState(false);
   return (
     <>
       <ContenedorIntercambiador>
@@ -31,142 +51,93 @@ const Intercambiador = ({ base }) => {
                 duration: 0.6,
                 ease: "easeIn",
               },
-              height: "10%",
+              height: "12%",
             }}
           >
-            <PestañaMenu></PestañaMenu>
             <MenuIntrucciones>
-              <button onClick={() => setIsVisible(!isVisible)}>hola</button>
-              <BotonContinuar>Continuar</BotonContinuar>
+              <TextoIntro>Selecciona que partes quieres forrar</TextoIntro>
+              <BotonContinuar>
+                <strong>Continuar</strong>
+              </BotonContinuar>
             </MenuIntrucciones>
           </ContenedorMenu>
+        )}
+        {isVisible && (
+          <ContenedorIconos>
+            <Icono1
+              src={icono_b}
+              alt='modulo'
+              style={{
+                top: "40%",
+                left: "17%",
+              }}
+            />
+            <Icono1
+              src={icono_b}
+              alt='encimera'
+              style={{
+                top: "45%",
+                left: "72%",
+              }}
+            />
+            <Icono1
+              src={icono_b}
+              alt='frente'
+              style={{
+                top: "70%",
+                left: "65%",
+              }}
+            />
+          </ContenedorIconos>
         )}
         <ContenedorImagenBase id='fondo' descripcion='Mueble lacado en blanco.'>
           <Imagen src={base} alt='fondo' />
         </ContenedorImagenBase>
-        {/* <ContenedorImagen
-        id='pon-muebleFondo'
-        descripcion='Mueble lacado en blanco.'
-      ></ContenedorImagen> */}
-        <ContenedorImagen
-          id='pon-alluref'
-          sitio='frontal'
-          //style={{ visibility: "visible" }}
-        >
+        <ContenedorImagen id='pon-alluref' sitio='frontal'>
           <Imagen src={frontales.alluref} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-berenf'
-          sitio='frontal'
-          //style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        {/* <ContenedorImagen>
           <Imagen src={frontales.berenf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-blancborgf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.blancborgf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-blancinvf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.blancinvf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-brazilf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.brazilf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-calacattaf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.calacattaf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-emef'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.emef} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-kendoCf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.kendoCf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-kendoLf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.kendoLf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-momaf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.momaf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-negrof'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.negrof} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-noonGf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.noonGf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-noonSf'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.noonSf} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-paladiof'
-          sitio='frontal'
-          // style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.paladiof} alt='frontal' />
         </ContenedorImagen>
-        <ContenedorImagen
-          id='pon-syracf'
-          sitio='frontal'
-          //style={{ visibility: "visible" }}
-          descripcion='Mueble en madera de roble malla fina.'
-        >
+        <ContenedorImagen>
           <Imagen src={frontales.syracf} alt='frontal' />
-        </ContenedorImagen>
+        </ContenedorImagen> */}
       </ContenedorIntercambiador>
     </>
   );
--- src/modules/bath/_AdaptBath/intercambiadorStyles.jsx
+++ src/modules/bath/_AdaptBath/intercambiadorStyles.jsx
@@ -10,15 +10,13 @@ export const ContenedorIntercambiador = styled.div`
 export const ContenedorImagenBase = styled.div`
   width: 100%;
   position: relative;
-  /* visibility: hidden; */
   top: 0;
 `;
 export const ContenedorImagen = styled.div`
   width: 100%;
   position: absolute;
-  visibility: hidden;
   top: 0;
-`;
+`;  
 export const Imagen = styled.img`
   width: 100%;
 `;
@@ -33,13 +31,11 @@ export const ContenedorMenu = styled(motion.div)`
   bottom: 0;
   height: 10%;
 `;
-export const PestañaMenu = styled.div`
-  background-color: #ff9a9a;
-`;
 export const MenuIntrucciones = styled.div`
   background-color: #303030;
   width: 100%;
   height: 100%;
+  display: grid;
 `;
 export const BotonExpandir = styled.button`
   background-color: transparent;
@@ -56,5 +52,62 @@ export const BotonExpandir = styled.button`
 export const BotonContinuar = styled.button`
   background-color: transparent;
   border: none;
+  color: white;
+  height: 100%;
+  font-size: 1rem;
   cursor: pointer;
+  align-self: center;
+  @media screen and (max-width: 1400px) {
+  }
+  @media screen and (max-width: 980px) {
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 12px;
+  }
+`;
+export const TextoIntro = styled.p`
+  color: white;
+  font-size: 1rem;
+  justify-self: center;
+  align-self: center;
+  @media screen and (max-width: 1400px) {
+  }
+  @media screen and (max-width: 980px) {
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 12px;
+  }
+`;
+
+export const ContenedorIconos = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 1;
+  @media screen and (max-width: 1400px) {
+  }
+  @media screen and (max-width: 980px) {
+  }
+  @media screen and (max-width: 480px) {
+    height: 92%;
+    width: 94%;
+  }
+`;
+export const Icono1 = styled.img`
+  position: absolute;
+  cursor: pointer;
+  @media screen and (max-width: 1400px) {
+  }
+  @media screen and (max-width: 980px) {
+    scale: 0.75;
+  }
+  @media screen and (max-width: 480px) {
+    scale: 0.5;
+  }
+  transition: all 0.2s linear;
+  :hover {
+    transform: rotate(-45deg);
+  }
 `;
