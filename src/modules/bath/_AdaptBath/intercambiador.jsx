8e59522a89e4e563eade86fcecb29ed367f3cb5f
H DLC <47111207+Enloch@users.noreply.github.com>
2023-01-31 16:03:16 +0100

    31/01 intercambiador menu backup 1

--- src/modules/bath/_AdaptBath/intercambiador.jsx
+++ src/modules/bath/_AdaptBath/intercambiador.jsx
@@ -1,141 +1,161 @@
-import React from "react";
+import React, { useState } from "react";
+
 import {
   ContenedorIntercambiador,
   ContenedorImagenBase,
   ContenedorImagen,
   Imagen,
+  ContenedorMenu,
+  PestañaMenu,
+  Menu,
+  BotonExpandir,
 } from "./intercambiadorStyles";
 import { frontales, modulos, laterales } from "./dataIntercambiador";
 const Intercambiador = ({ base }) => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
-    <ContenedorIntercambiador>
-      <ContenedorImagenBase id='fondo' descripcion='Mueble lacado en blanco.'>
-        <Imagen src={base} alt='fondo' />
-      </ContenedorImagenBase>
-      {/* <ContenedorImagen
+    <>
+      <ContenedorIntercambiador>
+        <BotonExpandir onClick={() => setIsVisible(!isVisible)}>
+          ^
+        </BotonExpandir>
+        {isVisible && (
+          <ContenedorMenu id='menu lateral' animate
+          >
+            <PestañaMenu></PestañaMenu>
+            <Menu>
+              <button onClick={() => console.log("hola")}>hola</button>
+            </Menu>
+          </ContenedorMenu>
+        )}
+        <ContenedorImagenBase id='fondo' descripcion='Mueble lacado en blanco.'>
+          <Imagen src={base} alt='fondo' />
+        </ContenedorImagenBase>
+        {/* <ContenedorImagen
         id='pon-muebleFondo'
         descripcion='Mueble lacado en blanco.'
       ></ContenedorImagen> */}
-      <ContenedorImagen
-        id='pon-alluref'
-        sitio='frontal'
-        //style={{ visibility: "visible" }}
-      >
-        <Imagen src={frontales.alluref} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-berenf'
-        sitio='frontal'
-        //style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.berenf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-blancborgf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.blancborgf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-blancinvf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.blancinvf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-brazilf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.brazilf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-calacattaf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.calacattaf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-emef'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.emef} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-kendoCf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.kendoCf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-kendoLf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.kendoLf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-momaf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.momaf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-negrof'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.negrof} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-noonGf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.noonGf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-noonSf'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.noonSf} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-paladiof'
-        sitio='frontal'
-        // style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.paladiof} alt='frontal' />
-      </ContenedorImagen>
-      <ContenedorImagen
-        id='pon-syracf'
-        sitio='frontal'
-        //style={{ visibility: "visible" }}
-        descripcion='Mueble en madera de roble malla fina.'
-      >
-        <Imagen src={frontales.syracf} alt='frontal' />
-      </ContenedorImagen>
-    </ContenedorIntercambiador>
+        <ContenedorImagen
+          id='pon-alluref'
+          sitio='frontal'
+          //style={{ visibility: "visible" }}
+        >
+          <Imagen src={frontales.alluref} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-berenf'
+          sitio='frontal'
+          //style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.berenf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-blancborgf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.blancborgf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-blancinvf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.blancinvf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-brazilf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.brazilf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-calacattaf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.calacattaf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-emef'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.emef} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-kendoCf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.kendoCf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-kendoLf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.kendoLf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-momaf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.momaf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-negrof'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.negrof} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-noonGf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.noonGf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-noonSf'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.noonSf} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-paladiof'
+          sitio='frontal'
+          // style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.paladiof} alt='frontal' />
+        </ContenedorImagen>
+        <ContenedorImagen
+          id='pon-syracf'
+          sitio='frontal'
+          //style={{ visibility: "visible" }}
+          descripcion='Mueble en madera de roble malla fina.'
+        >
+          <Imagen src={frontales.syracf} alt='frontal' />
+        </ContenedorImagen>
+      </ContenedorIntercambiador>
+    </>
   );
 };
 
--- src/modules/bath/_AdaptBath/intercambiadorStyles.jsx
+++ src/modules/bath/_AdaptBath/intercambiadorStyles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const ContenedorIntercambiador = styled.div`
   width: 100%;
@@ -20,4 +21,36 @@ export const ContenedorImagen = styled.div`
 `;
 export const Imagen = styled.img`
   width: 100%;
+`;
+
+export const ContenedorMenu = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  z-index: 2;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 10%;
+  background-color: #000;
+`;
+export const PestañaMenu = styled.div`
+  background-color: #ff9a9a;
+`;
+export const Menu = styled.div`
+  background-color: #420505;
+  width: 100%;
+  height: 100%;
+`;
+export const BotonExpandir = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  z-index: 1;
+  bottom: 1%;
+  width: 10%;
+  height: 10%;
+  left: 50%;
+  font-size: 2rem;
+  cursor: pointer;
 `;
