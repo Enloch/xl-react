e358fd2eb4358f778ddfa64965bfdd46b050e06f
Enloch <Hugodlcq@gmail.com>
2023-06-12 13:05:53 +0200

    12/06 Cambios csv y Scroll to top

--- src/components/GoTopBttn/ScrollToTop.jsx
+++ src/components/GoTopBttn/ScrollToTop.jsx
@@ -4,58 +4,44 @@ import styled from "styled-components";
 import "./styles.css";
 
 const ScrollToTop = () => {
-  const [showTopBtn, setShowTopBtn] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.screen.width > 2000) {
-        if (window.scrollY > 1290) {
-          setShowTopBtn(true);
-        } else {
-          setShowTopBtn(false);
-        }
-      }
-      if (window.screen.width <= 1930) {
-        if (window.scrollY > 900) {
-          setShowTopBtn(true);
-        } else {
-          setShowTopBtn(false);
-        }
-      }
-      if (window.screen.width <= 960) {
-        if (window.scrollY > 950) {
-          setShowTopBtn(true);
-        } else {
-          setShowTopBtn(false);
-        }
-      }
-      if (window.screen.width <= 480) {
-        if (window.scrollY > 780) {
-          setShowTopBtn(true);
-        } else {
-          setShowTopBtn(false);
-        }
-      }
-    });
-  }, []);
+  const [showTopBtn, setShowTopBtn] = useState(true);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     if (window.screen.width > 2000) {
+  //       if (window.scrollY > 1290) {
+  //         setShowTopBtn(true);
+  //       } else {
+  //         setShowTopBtn(false);
+  //       }
+  //     }
+  //     if (window.screen.width <= 1930) {
+  //       if (window.scrollY > 900) {
+  //         setShowTopBtn(true);
+  //       } else {
+  //         setShowTopBtn(false);
+  //       }
+  //     }
+  //     if (window.screen.width <= 960) {
+  //       if (window.scrollY > 950) {
+  //         setShowTopBtn(true);
+  //       } else {
+  //         setShowTopBtn(false);
+  //       }
+  //     }
+  //     if (window.screen.width <= 480) {
+  //       if (window.scrollY > 780) {
+  //         setShowTopBtn(true);
+  //       } else {
+  //         setShowTopBtn(false);
+  //       }
+  //     }
+  //   });
+  // }, []);
   const goToTop = () => {
-    if (window.screen.width > 2000) {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-    if (window.screen.width <= 1930) {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-    if (window.screen.width <= 480) {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
   return (
     <div className='top-to-btm'>
