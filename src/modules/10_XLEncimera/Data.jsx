76fa0762b0cabfd86c4a295608cc4fdd5dcb73bc
Enloch <Hugodlcq@gmail.com>
2023-09-07 08:45:22 +0200

    Cambios 07/09

--- src/modules/10_XLEncimera/Data.jsx
+++ src/modules/10_XLEncimera/Data.jsx
@@ -2,7 +2,7 @@ export const XlencimeraObj = {
   id: "bath",
   headLine: "XL Kitchen //",
   description:
-    "En XL Encimeras, nos apasiona la innovación y el diseño en el sector de la cocina . Nos especializamos en la producción encimeras en gran formato.",
+    "En XL KITCHEN, nos apasiona la innovación y el diseño en el sector de la cocina . Nos especializamos en la distribución de piedra sinterizada para  encimeras en espesores de 12 y 20 mm. Las mejores marcas y la mayor variedad de gama disponible en nuestras instalaciones",
   description2: "conozca más //",
   description3:
     "En XL Bath, nos apasiona la innovación y el diseño en el sector del baño. Nos especializamos en la producción de paneles, platos de ducha, encimeras y muebles de baño utilizando nuestras tablas de gran formato porcelánico de 6 y 12 mm de espesor.",
